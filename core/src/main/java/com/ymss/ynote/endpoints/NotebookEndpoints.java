b2bdd3655756191ece121eb503eb1329e7f1068a
Hui Deng <@Deng.(none)>
2013-12-06 20:50:30 +0800

    use ResteasyBootstrap instead of extending Application class

--- core/src/main/java/com/ymss/ynote/endpoints/NotebookEndpoints.java
+++ core/src/main/java/com/ymss/ynote/endpoints/NotebookEndpoints.java
@@ -1,7 +1,9 @@
 package com.ymss.ynote.endpoints;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -10,19 +12,31 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.ymss.ynote.note.Notebook;
 import com.ymss.ynote.storage.INotebookStorage;
 
+@Named
 @Path("/note/")
-public class NotebookEndpoint {
+public class NotebookEndpoints {
 
 	@Inject
 	private INotebookStorage notebookStorage;
 	
 	@POST
 	public Response createOne(
+			@Context HttpServletRequest hsr,
+			@Context UriInfo uriinfo,
+			@HeaderParam("Authorization") String pAuthentication,
+			@QueryParam("name") String name
+			) throws Exception
+	{
+		Notebook nbook = Notebook.newInstance(name);
+		notebookStorage.save(nbook);
+		return Response.ok().build();
+	}
+	
+	@GET
+	public Response getNotebooks(
 			@Context HttpServletRequest hsr,
 			@Context UriInfo uriinfo,
 			@HeaderParam("Authorization") String pAuthentication,
