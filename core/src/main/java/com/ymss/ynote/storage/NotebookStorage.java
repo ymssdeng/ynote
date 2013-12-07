66d99ad77d0980c99a0944d14abbecb003794321
Hui Deng <@Deng.(none)>
2013-12-07 21:46:16 +0800

    add hadoop common deprndency

---
+++ core/src/main/java/com/ymss/ynote/storage/NotebookStorage.java
@@ -0,0 +1,20 @@
+package com.ymss.ynote.storage;
+
+import javax.inject.Named;
+
+import org.springframework.stereotype.Component;
+
+import com.ymss.ynote.note.Notebook;
+
+@Named
+public class NotebookStorage implements INotebookStorage {
+
+	private static final int GG =0;
+	
+	@Override
+	public void save(Notebook notebook) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
