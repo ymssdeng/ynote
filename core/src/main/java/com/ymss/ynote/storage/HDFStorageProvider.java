0d014f780aeba737698785b58fa069150251e56f
U-ADVENT\mdeng <mdeng@mdeng_bj.advent.com>
2013-12-10 10:31:25 +0800

    add spring property-placeholder to handle properties file

--- core/src/main/java/com/ymss/ynote/storage/HDFStorageProvider.java
+++ core/src/main/java/com/ymss/ynote/storage/HDFStorageProvider.java
@@ -8,15 +8,19 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.springframework.beans.factory.annotation.Value;
 
 @Named
 public class HDFStorageProvider implements IStorageProvider {
 
 	private FileSystem hdfs = null;
+	@Value("${hdfs.url}")
+	private String hdfsUrl;
 
 	public HDFStorageProvider() throws IOException {
 		Configuration conf = new Configuration();
-		hdfs = FileSystem.getLocal(conf);
+		conf.set("fs.defaultFS", hdfsUrl);
+		hdfs = FileSystem.get(conf);
 	}
 
 	@Override
