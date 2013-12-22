bb4e001af0cc57f7411cee0dcc98d9a749411cf2
Hui Deng <tianma_25@163.com>
2013-12-22 20:44:05 +0800

    select key after insert

--- core/src/main/java/com/ymss/ynote/storage/provider/NotebookStorageProvider.java
+++ core/src/main/java/com/ymss/ynote/storage/provider/NotebookStorageProvider.java
@@ -1,11 +1,13 @@
 package com.ymss.ynote.storage.provider;
 
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.SelectKey;
 
 import com.ymss.ynote.note.Notebook;
 
 public interface NotebookStorageProvider {
 
-	@Insert("insert into notebook (name, category_id) values (#{name}, #{category.id})")
+	@Insert("insert into notebook (name, category_id) values (#{name}, #{category.id})") // equals to #{0.name}, #{param1.name}
+	@SelectKey(before = false, keyProperty = "id", resultType = Integer.class, statement = { "select last_insert_id()" })
 	void save(Notebook notebook);
 }
